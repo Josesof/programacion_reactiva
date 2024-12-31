d9ef569862a72278bba4a473f2279cedffe95987
josejhouar.mosquerar <josejhouar.mosqueraramirez@amaris.com>
2024-12-31 08:22:46 -0500

    acutalization project

--- microservicio_rastreador_consumidor/src/main/java/com/microservicio/rastreador/reactivo/model/Elemento.java
+++ microservicio_rastreador_consumidor/src/main/java/com/microservicio/rastreador/reactivo/model/Elemento.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
+
 @Data
 public class Elemento {
 
@@ -13,4 +13,36 @@ public class Elemento {
   private String categoria;
   private double precioUnitario;
   private String tienda;
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getCategoria() {
+    return categoria;
+  }
+
+  public void setCategoria(String categoria) {
+    this.categoria = categoria;
+  }
+
+  public double getPrecioUnitario() {
+    return precioUnitario;
+  }
+
+  public void setPrecioUnitario(double precioUnitario) {
+    this.precioUnitario = precioUnitario;
+  }
+
+  public String getTienda() {
+    return tienda;
+  }
+
+  public void setTienda(String tienda) {
+    this.tienda = tienda;
+  }
 }
--- microservicio_rastreador_consumidor/src/main/java/com/microservicio/rastreador/reactivo/service/ElementosServiceImpl.java
+++ microservicio_rastreador_consumidor/src/main/java/com/microservicio/rastreador/reactivo/service/ElementosServiceImpl.java
@@ -1,15 +1,28 @@
 package com.microservicio.rastreador.reactivo.service;
 
 import com.microservicio.rastreador.reactivo.model.Elemento;
+import io.netty.handler.codec.base64.Base64Encoder;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
+import java.util.Base64;
+@Slf4j
 @Service
 public class ElementosServiceImpl implements ElementosService {
-  String url1 = "http://localhost:8000";
+
+  private static final Logger log = LoggerFactory.getLogger(ElementosServiceImpl.class);
+  String url1 =  "http://localhost:8000";
   String url2 = "http://localhost:9000";
+  @Value("$user")
+  String user;
+  @Value("$pwd")
+  String pwd;
   @Override
   public Flux<Elemento> elementosPrecioMax(double precioMax) {
     Flux<Elemento> flux1 = catalogo(url1, "Amazon");
@@ -22,13 +35,19 @@ public class ElementosServiceImpl implements ElementosService {
     WebClient webClient = WebClient.create(url);
     return webClient
       .get()
-      .uri("/productos")
+      .uri("/allProductos")
       .accept(MediaType.APPLICATION_JSON)
+      .header("Authorization", "Basic "+getEncoderBase64Credentials("user", "user"))
       .retrieve()
       .bodyToFlux(Elemento.class)
       .map(e->{
         e.setTienda(tienda);
         return e;
       });
+  }
+  private String getEncoderBase64Credentials(String user, String pwd){
+     String credential = user+":"+pwd;
+    log.info("Iniciando la acción..."+credential);
+     return Base64.getEncoder().encodeToString(credential.getBytes());
   }
 }
